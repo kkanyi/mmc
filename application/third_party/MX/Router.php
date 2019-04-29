90462aeb2b8da4187a02a13be64e7fa469666dc5
Medford Kanyi <kkanyi@gmail.com>
2019-04-29 19:18:38 +0000

    updated to latest php and fixed errors

--- application/third_party/MX/Router.php
+++ application/third_party/MX/Router.php
@@ -235,8 +235,10 @@ class MX_Router extends CI_Router
 
 	public function set_class($class)
 	{
-		$suffix = $this->config->item('controller_suffix');
-		if (strpos($class, $suffix) === FALSE)
+		// $suffix = $this->config->item('controller_suffix');
+		$suffix = (string) $this->config->item('controller_suffix'); 
+		// if (strpos($class, $suffix) === FALSE)
+        if( $suffix && strpos($class, $suffix) === FALSE)
 		{
 			$class .= $suffix;
 		}
