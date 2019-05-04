3ebd604ea9763b809cf6a7e0a6bb2616d3dc946f
Medford Kanyi <kkanyi@gmail.com>
2019-05-04 02:13:05 +0000

    adding company logo

--- application/modules/users/controllers/Users.php
+++ application/modules/users/controllers/Users.php
@@ -33,9 +33,58 @@ class Users extends MX_Controller {
 
 	public function new()
 	{
-		$view_data = array();
-		$view_data['users'] = $this->ion_auth->users()->result(); // get all users
-		$this->page_data['content'] = $this->load->view('user_edit',$view_data,TRUE);
+		// $this->load->helper(array('form', 'url'));
+
+        // $this->load->library('form_validation');
+		$config = array(
+		        array(
+		                'field' => 'username',
+		                'label' => 'Username',
+		                'rules' => 'trim|required|is_unique[users.username]'
+		        ),
+		        array(
+		                'field' => 'first_name',
+		                'label' => 'First name',
+		                'rules' => 'trim|required',
+		        ),
+		        array(
+		                'field' => 'last_name',
+		                'label' => 'Last name',
+		                'rules' => 'trim|required',
+		        ),
+		        array(
+		                'field' => 'password',
+		                'label' => 'Password',
+		                'rules' => 'trim|required',
+		        ),
+		        array(
+		                'field' => 'password2',
+		                'label' => 'Password Confirmation',
+		                'rules' => 'trim|required|matches[password]'
+		        ),
+		        array(
+		                'field' => 'group',
+		                'label' => 'User group',
+		                'rules' => 'required'
+		        ),
+		        array(
+		                'field' => 'email',
+		                'label' => 'Email',
+		                'rules' => 'trim|required|is_unique[users.email]|valid_email'
+		        )
+		);
+
+		$this->form_validation->set_rules($config);
+
+        if ($this->form_validation->run() == FALSE){
+            // $this->load->view('myform');
+			$view_data = array();
+			// $view_data['users'] = $this->ion_auth->users()->result(); // get all users
+			$this->page_data['content'] = $this->load->view('user_edit',$view_data,TRUE);
+        }else{
+                $this->load->view('formsuccess');
+        }
+
 		// $this->page_data['content_title'] = 'Welcome';
 		$this->parser->parse('template/main', $this->page_data);
 	}
